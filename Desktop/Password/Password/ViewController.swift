bd5ab52040a597493c8683cb8b78fb52e5c10d46
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-02-22 14:00:47 +0000

    create new Password project

---
+++ Desktop/Password/Password/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Vladlens Kukjans on 22/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+        
+    }
+
+
+}
+
